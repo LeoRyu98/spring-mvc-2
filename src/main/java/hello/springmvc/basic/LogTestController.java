9c705a8d82060df7d8dfc0b9f4087b1c6b9c7ebb
kyeongwoo <drd9811@naver.com>
2022-04-02 19:07:28 +0900

    update 220402

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,38 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Contoller 는 반환 값이 String 이면 뷰 이름으로 인식 (뷰를 찾고 뷰가 렌더링)
+ * @RestContoller 는 반환 값으로 뷰를 찾는 것이 아니라, HTTP 메시지 바디에 바로 입력
+ * 따라서 실행 결과로 ok 메시지를 받을 수 있다. (@ResponseBody 관련)
+ */
+@Slf4j
+@RestController
+public class LogTestController {
+
+    // 로그 선언
+    // private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest(){
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+
+        // 로그를 사용하지 않아도 a+b 계산 로직이 먼저 실행됨, 이런 방식으로 사용하면 X
+        log.debug("String concat log=" + name);
+
+        return "ok";
+    }
+}
